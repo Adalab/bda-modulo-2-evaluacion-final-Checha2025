d8d90d4429d190e1cdae2a8e2cbe76cf641648e5
Checha <martinez.gandolfo.cecilia@gmail.com>
2026-05-21 01:22:10 +0200

    Preguntas

--- modulo_2_evaluacion._fase_2.sql
+++ modulo_2_evaluacion._fase_2.sql
@@ -6,7 +6,7 @@ CREATE TABLE tabla_generos (
 );
 CREATE TABLE tabla_peliculas (
     id_pelicula INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(150) NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
     año INT NOT NULL,
     duracion INT NOT NULL,
     adultos VARCHAR(2) NOT NULL,
@@ -20,6 +20,41 @@ SELECT * FROM tabla_generos;
 
 SELECT * FROM tabla_peliculas;
 
-SELECT COUNT (*) AS peliculas_mas_120
+SELECT COUNT(*) AS peliculas_mas_120
 FROM tabla_peliculas
 WHERE duracion> 120;
+
+SELECT COUNT(*) AS peliculas_adultos 
+FROM tabla_peliculas 
+WHERE adultos = 'Sí';
+
+SELECT titulo, año 
+FROM tabla_peliculas 
+ORDER BY año ASC 
+LIMIT 1;
+
+SELECT g.nombre_genero, ROUND(AVG(p.duracion), 2) AS promedio_duracion_min
+FROM tabla_peliculas p
+LEFT JOIN tabla_generos g ON p.id_genero = g.id_genero
+GROUP BY g.nombre_genero;
+
+SELECT año, COUNT(*) AS cantidad_peliculas 
+FROM tabla_peliculas 
+GROUP BY año 
+ORDER BY cantidad_peliculas DESC;
+
+SELECT año, COUNT(*) AS cantidad_peliculas 
+FROM tabla_peliculas 
+GROUP BY año 
+ORDER BY cantidad_peliculas DESC 
+LIMIT 1;
+
+SELECT g.nombre_genero, COUNT(p.id_pelicula) AS total_peliculas
+FROM tabla_generos g
+LEFT JOIN tabla_peliculas p ON g.id_genero = p.id_genero
+GROUP BY g.nombre_genero
+ORDER BY total_peliculas DESC
+
+SELECT *
+FROM tabla_peliculas
+WHERE titulo ILIKE '%godfather%';
